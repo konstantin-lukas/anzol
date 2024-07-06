01dd52bea7030ff30dd5dd2cbfb3b03118d1d73e
Konstantin Lukas <konstantin_lukas@web.de>
2024-07-06 19:39:43 +0200

    finish first version of useFetch and add test for full coverage

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,10 +1,39 @@
 import {useEffect, useState} from "react";
 
 /**
- * @description Fetches the provided URL and optionally parses the response.Aborts requests when a new request is
- * started before the previous has finished to prevent flickering of stale responses.
+ * @type {Object} FetchResult
+ * @property {any} data - The response to the fetch request. Type depends on selected parseType. Is null if fetch was
+ * unsuccessful
+ * @property {boolean} loading - Indicates if the fetch is still ongoing (i.e. has not finished). Wait for this to be
+ * true before reading other fields. Can be used to display a spinner while waiting for results.
+ * @property {boolean} ok - Whether the response was successful (status in the range 200-299) or not.
+ * @property {number} status - The HTTP status code. Is null before first fetch has completed.
  */
-const useFetch = (url: string, parseAs: "json" | "html" | "xml" | "text" = "json") => {
+type FetchResult = {
+    data: any,
+    loading: boolean,
+    ok: boolean,
+    status: number | null
+}
+
+/**
+ * @description Fetches the provided URL and optionally parses the response. Aborts requests when a new request is
+ * started before the previous has finished to prevent flickering of stale responses by default.
+ * @param url The resource to fetch
+ * @param parseType How to parse the result (determines type of returned data). Set to "response" if you don't want to
+ * extract the data automatically and receive the response object instead. Defaults to "json".
+ * @param options The options passed to the fetch function. Note: custom signals will be overwritten internally.
+ * @param discardStaleRequests Discards all responses except the last one made when set to true (default). This prevents
+ * flickering of data in the UI and ensures only the correct data is displayed. Only turn this off if you're really sure
+ * you want to.
+ * @return FetchResult
+ */
+const useFetch = (
+    url: string,
+    parseType: "json" | "html" | "xml" | "text" | "svg" | "response" = "json",
+    options: RequestInit = {},
+    discardStaleRequests = true
+): FetchResult => {
     const [status, setStatus] = useState<number | null>(null);
     const [ok, setOk] = useState<boolean>(false);
     const [loading, setLoading] = useState<boolean>(false);
@@ -16,31 +45,41 @@ const useFetch = (url: string, parseAs: "json" | "html" | "xml" | "text" = "json
         (async () => {
             setLoading(true);
             try {
-                const response = await fetch(url, { signal });
+                const response = await fetch(url, discardStaleRequests ? { ...options, signal } : options);
                 setStatus(response.status);
                 setOk(response.ok);
                 if (!response.ok) {
                     setData(null);
                 } else {
-                    if (parseAs === "json") {
+                    if (parseType === "json") {
                         setData(await response.json());
-                    } else if (parseAs === "html" || parseAs === "xml") {
+                    } else if (parseType === "html" || parseType === "xml" || parseType === "svg") {
                         const html = await response.text();
                         const parser = new DOMParser();
-                        const doc = parser.parseFromString(html, parseAs === "html" ? "text/html" : "text/xml");
+                        const doc = parser.parseFromString(html,
+                            parseType === "html" ? "text/html" :
+                                parseType === "svg" ? "image/svg+xml" :
+                                    "text/xml"
+                        );
                         setData(doc);
-                    } else {
+                    } else if (parseType === "text") {
                         setData(await response.text());
+                    } else {
+                        if (parseType !== "response")
+                            console.error("Unknown parse type: '" + parseType + "'\nReturned 'response' instead.");
+                        setData(response);
                     }
                 }
             } catch { } finally {
                 setLoading(false);
             }
         })();
-        return () => {
-            abortController.abort();
-        };
-    }, [url, parseAs]);
+        if (discardStaleRequests) {
+            return () => {
+                abortController.abort();
+            };
+        }
+    }, [url, parseType]);
     return {data, loading, ok, status};
 };
 
--- tests/useFetch.test.ts
+++ tests/useFetch.test.ts
@@ -4,6 +4,19 @@ import { useFetch } from "../src";
 beforeEach(() => {
     jest.clearAllMocks();
 });
+
+function preFetchAssertions(result: any) {
+    expect(result.current.data).toEqual(null);
+    expect(result.current.loading).toBe(true);
+    expect(result.current.ok).toBe(false);
+    expect(result.current.status).toEqual(null);
+}
+
+function postFetchAssertions(result: any) {
+    expect(result.current.ok).toBe(true);
+    expect(result.current.status).toEqual(200);
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+}
 test('should fetch and parse json', async () => {
     const mockData = { message: 'Success' };
     global.fetch = jest.fn(async () => {
@@ -16,37 +29,181 @@ test('should fetch and parse json', async () => {
     }) as jest.Mock;
 
     const {result} = await act(() => renderHook(() => useFetch(
-        "https://collectionapi.metmuseum.org/public/collection/v1/departments"
+        "/api"
     )));
 
-    expect(result.current.data).toEqual(null);
-    expect(result.current.loading).toBe(true);
-    expect(result.current.ok).toBe(false);
-    expect(result.current.status).toEqual(null);
+
+    preFetchAssertions(result);
     await waitFor(() => {
         expect(result.current.loading).toBe(false);
     });
     expect(result.current.data).toEqual(mockData);
-    expect(result.current.ok).toBe(true);
-    expect(result.current.status).toEqual(200);
-    expect(global.fetch).toHaveBeenCalledTimes(1);
+    postFetchAssertions(result);
 
 });
 
 test('should fetch and parse html', async () => {
+    const mockData = "<html lang='en'><body><h1>Hello, world!</h1></body></html>";
+    global.fetch = jest.fn(async () => {
+        await new Promise((r) => setTimeout(r, 0));
+        return Promise.resolve({
+            ok: true,
+            text: () => Promise.resolve(mockData),
+            status: 200
+        });
+    }) as jest.Mock;
 
+    const {result} = await act(() => renderHook(() => useFetch(
+        "/api",
+        "html"
+    )));
+
+
+    preFetchAssertions(result);
+    await waitFor(() => {
+        expect(result.current.loading).toBe(false);
+    });
+    expect(result.current.data).not.toBeInstanceOf(XMLDocument);
+    expect(result.current.data).toBeInstanceOf(Document);
+    expect((result.current.data as Document).body.querySelector('h1')?.innerHTML).toBe("Hello, world!");
+    postFetchAssertions(result);
+});
+
+test('should fetch and parse svg', async () => {
+    const mockData =
+        "<svg height=\"100\" width=\"100\" xmlns=\"http://www.w3.org/2000/svg\">\n" +
+        "  <circle r=\"45\" cx=\"50\" cy=\"50\" fill=\"red\" />\n" +
+        "</svg>";
+    global.fetch = jest.fn(async () => {
+        await new Promise((r) => setTimeout(r, 0));
+        return Promise.resolve({
+            ok: true,
+            text: () => Promise.resolve(mockData),
+            status: 200
+        });
+    }) as jest.Mock;
+
+    const {result} = await act(() => renderHook(() => useFetch(
+        "/api",
+        "svg"
+    )));
+
+
+    preFetchAssertions(result);
+    await waitFor(() => {
+        expect(result.current.loading).toBe(false);
+    });
+    expect(result.current.data).toBeInstanceOf(Document);
+    expect((result.current.data as XMLDocument).querySelector('circle')?.getAttribute('fill')).toBe("red");
+    postFetchAssertions(result);
 });
 
 test('should fetch and parse xml', async () => {
+    const mockData = "<root><info>very interesting</info></root>";
+    global.fetch = jest.fn(async () => {
+        await new Promise((r) => setTimeout(r, 0));
+        return Promise.resolve({
+            ok: true,
+            text: () => Promise.resolve(mockData),
+            status: 200
+        });
+    }) as jest.Mock;
 
+    const {result} = await act(() => renderHook(() => useFetch(
+        "/api",
+        "xml"
+    )));
+
+
+    preFetchAssertions(result);
+    await waitFor(() => {
+        expect(result.current.loading).toBe(false);
+    });
+    expect(result.current.data).toBeInstanceOf(Document);
+    expect((result.current.data as XMLDocument).querySelector('info')?.textContent).toBe("very interesting");
+    postFetchAssertions(result);
 });
 
 test('should fetch and parse text', async () => {
+    const mockData = "<root><info>very interesting</info></root>";
+    global.fetch = jest.fn(async () => {
+        await new Promise((r) => setTimeout(r, 0));
+        return Promise.resolve({
+            ok: true,
+            text: () => Promise.resolve(mockData),
+            status: 200
+        });
+    }) as jest.Mock;
 
+    const {result} = await act(() => renderHook(() => useFetch(
+        "/api",
+        "text"
+    )));
+
+    preFetchAssertions(result);
+    await waitFor(() => {
+        expect(result.current.loading).toBe(false);
+    });
+    expect(typeof result.current.data).toBe("string");
+    expect(result.current.data).toBe(mockData);
+    postFetchAssertions(result);
 });
 
-test('should gracefully handle errors', async () => {
+test('should fetch and parse text', async () => {
+    const mockData = "<root><info>very interesting</info></root>";
+    global.fetch = jest.fn(async () => {
+        await new Promise((r) => setTimeout(r, 0));
+        return Promise.resolve({
+            ok: true,
+            text: () => Promise.resolve(mockData),
+            status: 200
+        });
+    }) as jest.Mock;
 
+    console.error = jest.fn();
+
+    const {result} = await act(() => renderHook(() => useFetch(
+        "/api",
+        "svf" as "svg"
+    )));
+
+    preFetchAssertions(result);
+    await waitFor(() => {
+        expect(result.current.loading).toBe(false);
+    });
+    expect(typeof result.current.data).toBe("object");
+    expect(await result.current.data.text()).toBe(mockData);
+    expect(console.error).toHaveBeenCalledTimes(1);
+    postFetchAssertions(result);
+});
+
+
+test('should fetch and not parse the response', async () => {
+    const mockData = "I'm a teapot.";
+    global.fetch = jest.fn(async () => {
+        await new Promise((r) => setTimeout(r, 0));
+        return Promise.resolve({
+            ok: true,
+            statusText: mockData,
+            status: 201
+        });
+    }) as jest.Mock;
+
+    console.error = jest.fn();
+
+    const {result} = await act(() => renderHook(() => useFetch(
+        "/api",
+        "response"
+    )));
+
+    preFetchAssertions(result);
+    await waitFor(() => {
+        expect(result.current.loading).toBe(false);
+    });
+    expect(result.current.data?.ok).toBe(true);
+    expect(result.current.data?.status).toBe(201);
+    expect(result.current.data?.statusText).toBe(mockData);
+    expect(console.error).toHaveBeenCalledTimes(0);
 });
 
 test('should discard stale requests', async () => {
@@ -79,10 +236,7 @@ test('should discard stale requests', async () => {
         });
     });
 
-    expect(result.current.data).not.toEqual(mockData1);
-    expect(result.current.loading).toBe(true);
-    expect(result.current.ok).toBe(false);
-    expect(result.current.status).toEqual(null);
+    preFetchAssertions(result);
 
     rerender(secondUrl);
 
@@ -93,4 +247,72 @@ test('should discard stale requests', async () => {
     expect(result.current.ok).toBe(true);
     expect(result.current.status).toEqual(200);
     expect(global.fetch).toHaveBeenCalledTimes(2);
+});
+
+
+test('should not discard stale requests when asked to do so', async () => {
+    const mockData1 = { message: 'Success1' };
+    const mockData2 = { message: 'Success2' };
+    const firstUrl = "https://first-url.com";
+    const secondUrl = "https://second-url.com";
+
+    global.fetch = jest.fn(async (url: string) => {
+        if (url === firstUrl) {
+            await new Promise((r) => setTimeout(r, 100));
+            return Promise.resolve({
+                ok: true,
+                json: () => Promise.resolve(mockData1),
+                status: 200
+            });
+        } else {
+            return Promise.resolve({
+                ok: true,
+                json: () => Promise.resolve(mockData2),
+                status: 200
+            });
+        }
+    }) as jest.Mock;
+
+    const { result, rerender } = await act(async () => {
+        // @ts-ignore
+        return renderHook((url: string) => useFetch(url, "json", {}, false), {
+            initialProps: firstUrl
+        });
+    });
+
+    preFetchAssertions(result);
+
+    rerender(secondUrl);
+
+    await new Promise((r) => setTimeout(r, 200));
+    await waitFor(() => {
+        expect(result.current.loading).toBe(false);
+    });
+    expect(result.current.data).toEqual(mockData1);
+    expect(result.current.ok).toBe(true);
+    expect(result.current.status).toEqual(200);
+    expect(global.fetch).toHaveBeenCalledTimes(2);
+});
+
+test('should return no data on error', async () => {
+    global.fetch = jest.fn(async () => {
+        await new Promise((r) => setTimeout(r, 0));
+        return Promise.resolve({
+            ok: false,
+            status: 404
+        });
+    }) as jest.Mock;
+
+    const {result} = await act(() => renderHook(() => useFetch(
+        "/api",
+    )));
+
+    preFetchAssertions(result);
+    await waitFor(() => {
+        expect(result.current.loading).toBe(false);
+    });
+    expect(result.current.data).toEqual(null);
+    expect(result.current.ok).toBe(false);
+    expect(result.current.status).toEqual(404);
+    expect(global.fetch).toHaveBeenCalledTimes(1);
 });
